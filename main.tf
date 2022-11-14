734c432df88f337b04a8cd6e5f52b8715ec4be93
Sandor Kosztka <kosztkas@users.noreply.github.com>
2022-11-14 19:12:02 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+locals {
+  group_names = var.group_names
+  vms          = var.vm_suffix
+
+  # Nested loop over both lists, and flatten the result.
+  vm_names = distinct(flatten([
+    for groupnames in local.group_names : [
+      for vm in local.vms : {
+        groupname = groupnames
+        vm        = vm
+      }
+    ]
+  ]))
+}
+
+resource "aws_instance" "instances" {
+  for_each      = { for entry in local.vm_names: "${entry.groupname}.${entry.vm}" => entry }
+  instance_type               = var.instance_type
+  ami                         = var.ami_id
+  key_name                    = "${each.value.groupname}-key"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = [ var.security-group-id ]
+  subnet_id                   = var.subnet-id
+  
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    Name  = "${each.value.groupname}-${each.value.vm}"
+    Group = each.value.groupname
+  }
+}
